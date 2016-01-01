4cd4dc1980024e5e855a3d9b72939f029a8a9dad
CJLorax <cjones@wa75945.cgd.local>
2016-01-01 16:54:14 -0600

    works for now

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,10 @@
 #if defined (__APPLE__)
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_net.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_mixer/SDL_mixer.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#include <SDL2_net/SDL_net.h>
 
 #endif
 
